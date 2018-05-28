e357372046cd213f1e6d56d9780b09c7ebd8bb68
Alain Boudard <coco.boudard@gmail.com>
2018-05-28 16:43:43 +0200

    update NgRx Data 6.0.0-beta 6 / breaking changes with EntityActions

--- src/angular/src/app/services/toast.service.ts
+++ src/angular/src/app/services/toast.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
-import { EntityActions, OP_ERROR, OP_SUCCESS } from 'ngrx-data';
+import { EntityAction, OP_ERROR, OP_SUCCESS, ofEntityOp } from 'ngrx-data';
 import { ToastrService } from 'ngx-toastr';
+import { Actions } from '@ngrx/effects';
+import { filter } from 'rxjs/operators';
 
 @Injectable()
 export class ToastService {
-  constructor(actions$: EntityActions, private toastrservice: ToastrService) {
+  constructor(actions$: Actions, private toastrservice: ToastrService) {
     actions$
-      .where(ea => ea.op.endsWith(OP_SUCCESS) || ea.op.endsWith(OP_ERROR))
+      .pipe(
+        ofEntityOp(),
+        filter(
+          (ea: EntityAction) =>
+            ea.op.endsWith(OP_SUCCESS) || ea.op.endsWith(OP_ERROR)
+        )
+      )
       // this service never dies so no need to unsubscribe
       .subscribe(action =>
         // this is a hack against "expression changed" bug listed here https://github.com/scttcper/ngx-toastr/issues/160
